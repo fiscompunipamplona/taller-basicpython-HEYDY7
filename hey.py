cda02b7e2bf50514d276d307527d58bd3719a894
jagamboa95 <alexgamboa8@gmail.com>
2020-03-03 07:15:19 -0500

    taller 3 de marzo

---
+++ hey.py
@@ -0,0 +1,24 @@
+
+print("hola")
+x=5
+y=6
+z=8
+print(x+y)
+print(y-x)
+print(x*y)
+print(y/x)
+print(x+2*y)
+print(x-y/2)
+print(2*x**3)
+print(x/2*y)
+print(x, y,z, sep="...")
+print(x,"\n",y)
+x=input("entre el valor para x:")
+print(x)
+input ("Entre el valor para el timpo t: ")
+input ("Entre el valor para la altura h: ")
+g=9.8
+y=(0.5*g*t**2)
+x=(y-h)
+print(x)
+
